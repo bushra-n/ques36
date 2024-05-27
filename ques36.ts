ae140fa95e4282f89c150a7faf8fadeebe878529
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-27 10:01:10 +0500

    Add files via upload

---
+++ ques36.ts
@@ -0,0 +1,39 @@
+// Task 36 T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that 
+// should be printed on the shirt. The function should print a sentence summarizing the size of the 
+// shirt and the message printed on it. Call the function.
+
+function make_shirt(size:string,text:string):void {
+console.log(`You order a ${size} shirt that says ${text} `)
+}
+make_shirt('Medium','"I love typescript"')
+make_shirt('large','"Fear does not stop death,it stops life"')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
